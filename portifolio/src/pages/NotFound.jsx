5b7069d6a8491ca0d1dfca3e20085e4c99a425f5
heitorFS <heitor.freitasssilva@gmail.com>
2023-12-11 19:16:15 -0300

    not found page developed

--- portifolio/src/pages/NotFound.jsx
+++ portifolio/src/pages/NotFound.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import '../css/NotFound.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBomb } from "@fortawesome/free-solid-svg-icons";
 
 const NotFound = () => {
     return (
         <div className="not-found-container">
-            Not Found
+            <FontAwesomeIcon className="not-found-bomb" icon={faBomb} />
+            <div className="not-found-text">
+                <div className="not-found-title">Oops, I don't think this exists</div>
+                <div>I guess this page exploded on the road to your device.</div>
+            </div>
         </div>
     );
 };
